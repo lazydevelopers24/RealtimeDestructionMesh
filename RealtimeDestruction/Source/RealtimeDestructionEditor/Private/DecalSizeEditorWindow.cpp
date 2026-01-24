3f540344c04b1842820b5e7a5ebe4b0edeb0a857
Jinhyeok Choi <reality331@khu.ac.kr>
2026-01-24 23:37:43 +0900

    chore: Add and edit copyright comments
    [git-p4: depot-paths = "//depot/Main/RealDestructMesh/Plugins/RealtimeDestruction/": change = 559]

--- RealtimeDestruction/Source/RealtimeDestructionEditor/Private/DecalSizeEditorWindow.cpp
+++ RealtimeDestruction/Source/RealtimeDestructionEditor/Private/DecalSizeEditorWindow.cpp
@@ -1,4 +1,13 @@
-﻿#include "DecalSizeEditorWindow.h"
+// Copyright (c) 2026 LazyDevelopers <lazydeveloper24@gmail.com>. All rights reserved.
+// This plugin is distributed under the Fab Standard License.
+//
+// This product was independently developed by us while participating in the Epic Project, a developer-support
+// program of the KRAFTON JUNGLE GameTech Lab. All rights, title, and interest in and to the product are exclusively
+// vested in us. Krafton, Inc. was not involved in its development and distribution and disclaims all representations
+// and warranties, express or implied, and assumes no responsibility or liability for any consequences arising from
+// the use of this product.
+
+#include "DecalSizeEditorWindow.h"
 #include "DecalSizeEditorViewport.h"
 #include "Components/DestructionProjectileComponent.h"
 
